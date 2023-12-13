19c7964c4d72060c47d119f1f78f863468fbd059
Luiz Felipe Carminatti <felipecarminatti28@gmail.com>
2023-12-13 15:52:37 -0300

    Atualização2

---
+++ Gerador/app.js
@@ -0,0 +1,31 @@
+let codigo = document.getElementById('codigo')
+let opcao = document.getElementById('opcao')
+let qtd = document.getElementById('qtd')
+let tipo = document.getElementById('tipo')
+
+function gerador() {
+    let arrayNovo = ''
+    if (opcao.value == 'array') {
+        if (tipo.value == 'numero') {
+            for (let i = 0; i < qtd.value; i++) {
+                let aleatorio = Math.random() * (0 + 100) + 0
+                arrayNovo += `${Math.floor(aleatorio)}, `
+            }
+            codigo.innerHTML = `let valores = [${arrayNovo}]`
+        } else if (tipo.value == 'nome') {
+            let nomes = ['Sofia', 'Isabella', 'Camila', 'Mariana', 'Daniela', 'Gabriela', 'Renata', 'Paula', 'Amanda', 'Carolina', 'Rafaela', 'Adriana', 'Michelle', 'Juliana', 'Julia', 'Ana', 'Alessandra', 'Nicolás', 'Diego', 'Samuel', 'Daniel', 'Lucas', 'Gabriel', 'David', 'Emmanuel', 'Leonardo', 'Felipe', 'Carlos', 'Fernando', 'Luciano', 'Francisco', 'Rafael', 'Alex', 'Jesús', 'Camilo', 'Elías']
+            let aleatorio = ' '
+            for (let i = 0; i < qtd.value; i++) {
+                aleatorio = Math.floor(Math.random() * nomes.length)
+                arrayNovo += `${nomes[aleatorio]},`
+                codigo.innerHTML = `let nomes = [${arrayNovo}]`
+            }
+        }
+    } else if (opcao.value == 'objeto') {
+        for (let i = 0; i < qtd.value; i++) {
+            let aleatorio = Math.random() * (0 + 100) + 0
+            arrayNovo += `${Math.floor(aleatorio)}, `
+        }
+        codigo.innerHTML = `let valores = {${arrayNovo}}`
+    }
+}
