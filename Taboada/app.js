bf05a770e460605ca66351dadffd1dbcc9b1e641
Luiz Felipe Carminatti <felipecarminatti28@gmail.com>
2023-12-22 15:50:42 -0300

    Atualização5

---
+++ Taboada/app.js
@@ -0,0 +1,19 @@
+let res = document.getElementById('res')
+let valor = document.getElementById('valor')
+valor.addEventListener('input', function () {
+    let ul = document.createElement('ul')
+    res.innerHTML = ''
+    ul.innerHTML = `
+        <li>1 x ${valor.value} = ${1 * valor.value}</li>
+        <li>2 x ${valor.value} = ${2 * valor.value}</li>
+        <li>3 x ${valor.value} = ${3 * valor.value}</li>
+        <li>4 x ${valor.value} = ${4 * valor.value}</li>
+        <li>5 x ${valor.value} = ${5 * valor.value}</li>
+        <li>6 x ${valor.value} = ${6 * valor.value}</li>
+        <li>7 x ${valor.value} = ${7 * valor.value}</li>
+        <li>8 x ${valor.value} = ${8 * valor.value}</li>
+        <li>9 x ${valor.value} = ${9 * valor.value}</li>
+        <li>10 x ${valor.value} = ${10 * valor.value}</li>
+    `
+    res.appendChild(ul)
+})
---
+++ minMax/app.js
@@ -0,0 +1,9 @@
+function verificar() {
+    let valor = document.getElementById('valor').value
+    let res = document.getElementById('res')
+    let total = 1
+    for (let i = 1; i <= valor; i++) {
+        total *= i
+    }
+    res.innerHTML = total
+}
